971189ad1db51e8718d52585e74a0947ad3829e5
mjvillamayor <mjvillamayor123@gmail.com>
2025-03-06 18:32:28 -0800

    Housekeeping changes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const db = require('./database');
+
 const app = express();
 app.use(express.json());
 
@@ -6,4 +8,5 @@ app.get('/', (req, res) => {
     res.send('Medication Management System API is running');
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
