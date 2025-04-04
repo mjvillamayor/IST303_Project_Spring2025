18faa09d7e65e2ceeef306511cf0e0a14c81cb9b
Ben <bmorehead20@gmail.com>
2025-04-03 20:19:02 -0700

    updated sql file

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+USE medical_system;
+
 CREATE TABLE patients (
     patient_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
@@ -56,6 +58,21 @@ CREATE TABLE alerts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE PatientMedicalHistory (
+    history_id INT AUTO_INCREMENT PRIMARY KEY,
+    patient_id BIGINT UNSIGNED NOT NULL,
+    visit_date DATE NOT NULL,
+    diagnosis VARCHAR(255),
+    treatment TEXT,
+    medications TEXT,
+    provider_id BIGINT UNSIGNED,  -- Adjusted to match Providers table
+    notes TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (patient_id) REFERENCES Patients(patient_id),
+    FOREIGN KEY (provider_id) REFERENCES Providers(provider_id)
+);
+
 -- Create roles
 CREATE ROLE admin;
 CREATE ROLE provider;
@@ -82,10 +99,11 @@ CREATE USER 'john_doe'@'localhost' IDENTIFIED BY 'patient_password';
 GRANT patient TO 'john_doe'@'localhost';
 
 -- Ensure users activate their roles on login
-SET DEFAULT ROLE admin FOR 'admin_user'@'localhost';
-SET DEFAULT ROLE provider FOR 'dr_smith'@'localhost';
-SET DEFAULT ROLE patient FOR 'john_doe'@'localhost';
+SET DEFAULT ROLE admin TO 'admin_user'@'localhost';
+SET DEFAULT ROLE provider TO 'dr_smith'@'localhost';
+SET DEFAULT ROLE patient TO 'john_doe'@'localhost';
 
+-- Give DB access
 CREATE USER 'mark'@'192.168.12.249' IDENTIFIED BY 'MarkCGU';
 GRANT ALL PRIVILEGES ON medical_system.* TO 'mark'@'192.168.12.249';
 FLUSH PRIVILEGES;
@@ -96,4 +114,8 @@ FLUSH PRIVILEGES;
 
 CREATE USER 'jade'@'76.89.206.90' IDENTIFIED BY 'JadeCGU';
 GRANT ALL PRIVILEGES ON medical_system.* TO 'jade'@'76.89.206.90';
+FLUSH PRIVILEGES;
+
+CREATE USER 'test'@'192.168.1.250' IDENTIFIED BY 'testcase';
+GRANT ALL PRIVILEGES ON medical_system.* TO 'test'@'192.168.1.250';
 FLUSH PRIVILEGES;
